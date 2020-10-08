34f599ebffa116ec750f16ff6f6468191e9139e1
Ken Vilar <kenvilar@gmail.com>
2020-10-08 11:03:02 +0800

    Add content json for articles

---
+++ src/pages/article-content.js
@@ -0,0 +1,46 @@
+const articleContent = [
+  {
+    name: 'learn-react',
+    title: 'The Fastest Way to Learn React',
+    content: [
+      `Blogs are essential for every business. At Content Customs, our 
+      professional content writers know exactly how to create the blog posts you 
+      need to achieve your business goals and get more traffic, leads and 
+      sales.`,
+      `Since the beginning of the internet, millions and millions and millions 
+      of blogs have been created. Many have died due to lost interest or their 
+      owners giving up on the idea, while others have thrived and continue to 
+      grow, making money and earning their owners a steady income. It’s a 
+      constant evolution of content that keeps people coming back for more, 
+      especially if these blogs contact highly resourceful material that people 
+      find useful and interesting.`,
+    ],
+  },
+  {
+    name: 'learn-angular',
+    title: 'The Fastest Way to Learn Angular',
+    content: [
+      `Since the beginning of the internet, millions and millions and millions 
+      of blogs have been created. Many have died due to lost interest or their 
+      owners giving up on the idea, while others have thrived and continue to 
+      grow, making money and earning their owners a steady income. It’s a 
+      constant evolution of content that keeps people coming back for more, 
+      especially if these blogs contact highly resourceful material that people 
+      find useful and interesting.`,
+    ],
+  },
+  {
+    name: 'learn-vue',
+    title: 'The Fastest Way to Learn VueJS',
+    content: [
+      `The great thing about this blog is the conversational tone and the real 
+      person behind the brand. I think it’s inspiring to other mums to see 
+      someone like them making such a difference in other people’s lives by 
+      creating amazingly useful content that is 100% actionable. Also it may 
+      inspire mums to set up their own blog and to write about their experiences 
+      as a mother and a wife in the 21st century.`,
+    ],
+  },
+];
+
+export default articleContent;
