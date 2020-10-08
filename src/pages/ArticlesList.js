7d01025db3f0ccbad19a832573f9b1142da2d1e1
Ken Vilar <kenvilar@gmail.com>
2020-10-08 11:27:58 +0800

    List all articles in ArticlesList.js

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import articleContent from './article-content';
 
 const ArticlesList = () => (
   <>
     <h1>Articles</h1>
+    {articleContent.map(article => (
+      <h3>{article.title}</h3>
+    ))}
   </>
 );
 
