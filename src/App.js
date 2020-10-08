edb5d781fc501968d8437b47f137e30721744784
Ken Vilar <kenvilar@gmail.com>
2020-10-08 10:03:46 +0800

    Import new pages to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import {
   Route,
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
 import './App.css';
 
 function App() {
