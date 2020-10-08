29973bfb73af16117a4ce3a211dd7aef6a393d38
Ken Vilar <kenvilar@gmail.com>
2020-10-08 11:25:39 +0800

    Modify NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,25 +2,18 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const NavBar = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link to="/" className="navbar-brand">My Blog</Link>
-    <button className="navbar-toggler" type="button" data-toggle="collapse"
-            data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
-            aria-expanded="false" aria-label="Toggle navigation">
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item active">
-          <Link to="/" className="nav-link">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">About</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/articles-list" className="nav-link">Articles</Link>
-        </li>
-      </ul>
-    </div>
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/articles-list">Articles</Link>
+      </li>
+    </ul>
   </nav>
 );
 
