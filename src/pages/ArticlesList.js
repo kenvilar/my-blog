1ee2dfe781e80daafc317a021865c68708b25e69
Ken Vilar <kenvilar@gmail.com>
2020-10-10 10:01:46 +0800

    Linking the articles list

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import articleContent from './article-content';
+import {Link} from 'react-router-dom';
 
 const ArticlesList = () => (
   <>
     <h1>Articles</h1>
     {articleContent.map(article => (
-      <h3>{article.title}</h3>
+      <Link to={'/article/' + article.name}>
+        <h3>{article.title}</h3>
+      </Link>
     ))}
   </>
 );
